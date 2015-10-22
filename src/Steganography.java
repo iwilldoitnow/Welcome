c5e96800a0e40995ae2f7ce1cc4b4cb68d3aae4b
iwilldoitnow <s.komisarczyk@gmail.com>
2015-10-22 23:06:40 +0200

    Add menu

--- src/Steganography.java
+++ src/Steganography.java
@@ -6,10 +6,30 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
 import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JPasswordField;
+import javax.swing.DropMode;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Steganography {
 
 	private JFrame frame;
+	private JTextField txtSteganografia;
+	private JMenuBar menuBar;
+	private JMenu mnNewMenu;
+	private JMenuItem mntmView;
+	private JMenu mnEdit;
+	private JButton btnOpen;
+	private JButton btnSave;
+	private JButton btnNev;
 
 	/**
 	 * Launch the application.
@@ -39,11 +59,40 @@ public class Steganography {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 627, 425);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
-		frame.getContentPane().add(rdbtnNewRadioButton, BorderLayout.WEST);
+		txtSteganografia = new JTextField();
+		txtSteganografia.setBackground(Color.BLACK);
+		txtSteganografia.setEnabled(false);
+		txtSteganografia.setEditable(false);
+		txtSteganografia.setForeground(Color.WHITE);
+		txtSteganografia.setHorizontalAlignment(SwingConstants.CENTER);
+		txtSteganografia.setText("Hide your information in a graphic file");
+		frame.getContentPane().add(txtSteganografia, BorderLayout.NORTH);
+		txtSteganografia.setColumns(10);
+		
+		menuBar = new JMenuBar();
+		menuBar.setToolTipText("File\r\nExit");
+		frame.setJMenuBar(menuBar);
+		
+		mnNewMenu = new JMenu("File");
+		menuBar.add(mnNewMenu);
+		
+		btnOpen = new JButton("Open");
+		mnNewMenu.add(btnOpen);
+		
+		btnSave = new JButton("Save");
+		mnNewMenu.add(btnSave);
+		
+		btnNev = new JButton("New");
+		mnNewMenu.add(btnNev);
+		
+		mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+		
+		mntmView = new JMenuItem("View");
+		menuBar.add(mntmView);
 	}
 
 }
