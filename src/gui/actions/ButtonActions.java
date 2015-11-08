cb587aba91500a039cf9a7b8884345079610782f
iwilldoitnow <s.komisarczyk@gmail.com>
2015-11-08 22:44:01 +0100

    brak

--- src/gui/actions/ButtonActions.java
+++ src/gui/actions/ButtonActions.java
@@ -6,7 +6,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class ButtonActions {
 	
 	public static void openGraphicFile() {
-//		System.out.println("The bmp file will open here...\nActually a select window should appear.");
 		JFileChooser chooser = new JFileChooser();
 	    FileNameExtensionFilter filter = new FileNameExtensionFilter(
 	        "BMP Images", "bmp");
