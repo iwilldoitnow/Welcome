1929e57ebf7a27143b31e1da98f20bda2b14cdd2
dev <dev@dev-PC>
2015-11-08 22:20:56 +0100

    JFileChooser handling the Open button in menu.

--- src/gui/actions/ButtonActions.java
+++ src/gui/actions/ButtonActions.java
@@ -1,9 +1,21 @@
 package gui.actions;
 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class ButtonActions {
 	
 	public static void openGraphicFile() {
-		System.out.println("The bmp file will open here...\nActually a select window should appear.");
+//		System.out.println("The bmp file will open here...\nActually a select window should appear.");
+		JFileChooser chooser = new JFileChooser();
+	    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+	        "BMP Images", "bmp");
+	    chooser.setFileFilter(filter);
+	    int returnVal = chooser.showOpenDialog(chooser);
+	    if(returnVal == JFileChooser.APPROVE_OPTION) {
+	       System.out.println("You chose to open this file: " +
+	            chooser.getSelectedFile().getName());
+	    }
 	}
 
 }
